c164166e00cabc29147db3583a8cabf23a485434
Ardi <70941433+Ardinio@users.noreply.github.com>
2023-06-15 11:42:58 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SImpel_Calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Sum Calculator!");
+
+        Console.Write("Enter the first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = num1 + num2;
+        Console.WriteLine("The sum of {0} and {1} is {2}.", num1, num2, sum);
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
+
