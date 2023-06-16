d530e11a823dc8ae806590410581934f25c2e08b
Ardi <70941433+Ardinio@users.noreply.github.com>
2023-06-16 11:34:03 +0200

    Made it more like best practices

--- Program.cs
+++ Program.cs
@@ -8,17 +8,36 @@ class Program
     {
         Console.WriteLine("Welcome to the Sum Calculator!");
 
-        Console.Write("Enter the first number: ");
-        int num1 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Enter the second number: ");
-        int num2 = Convert.ToInt32(Console.ReadLine());
+        int num1 = ReadIntegerInput("Enter the first number: ");
+        int num2 = ReadIntegerInput("Enter the second number: ");
 
         int sum = num1 + num2;
         Console.WriteLine("The sum of {0} and {1} is {2}.", num1, num2, sum);
 
         Console.WriteLine("Press any key to exit.");
         Console.ReadKey();
+    }
+
+    static int ReadIntegerInput(string message)
+    {
+        int number;
+        bool isValidInput = false;
+
+        do
+        {
+            Console.Write(message);
+            string? input = Console.ReadLine();
+
+            isValidInput = int.TryParse(input, out number);
+
+            if (!isValidInput)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+        while (!isValidInput);
+
+        return number;
     }
 }
 
